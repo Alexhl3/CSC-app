0af3ed611e9ed5cf39d331440d568bf42f857373
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-08-10 11:11:12 -0400

    new product form

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -7,13 +7,32 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select '.product', 2
   end
-  
+
   test 'render detail product view' do
     get product_path(products(:machine1))
-    
+
     assert_response :success
     assert_select '.title', 'Maquina HH22O'
     assert_select '.description', 'Maquina bien cuidada.'
     assert_select '.price', '$200'
+  end
+
+  test 'render new product from' do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to create new product' do
+    post products_path, params: {
+      product: {
+        title: 'maquina XXJ4',
+        description: 'Maquina en buen estado.',
+        price: 230
+      }
+    }
+
+    assert_redirected_to products_path
   end
 end
