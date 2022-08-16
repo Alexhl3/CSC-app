b3c49d17e1c4a90f122635e4e42391ff650b25ae
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-08-16 15:46:12 -0400

    added locales to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,7 +16,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
       if @category.save
-        redirect_to categories_url, notice: "Category was successfully created."
+        redirect_to categories_url, notice: t('.created')
       else
         render :new, status: :unprocessable_entity
       end
@@ -24,7 +24,7 @@ class CategoriesController < ApplicationController
 
   def update
       if category.update(category_params)
-        redirect_to categories_url, notice: "Category was successfully updated."
+        redirect_to categories_url, notice: t('.updated')
       else
         render :edit, status: :unprocessable_entity
       end
@@ -32,7 +32,7 @@ class CategoriesController < ApplicationController
 
   def destroy
     category.destroy
-    redirect_to categories_url, notice: "Category was successfully destroyed."
+    redirect_to categories_url, notice: t('.destroyed')
   end
 
   private
