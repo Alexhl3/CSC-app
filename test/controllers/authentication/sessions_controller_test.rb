bcfe76741703822c3eaa1a4aafe4373079cddbcd
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-08-18 16:02:54 -0400

    user login

---
+++ test/controllers/authentication/sessions_controller_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class Authentication::SessionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:pedro)
+  end
+
+  test "new_user_url" do
+    get new_session_url
+
+    assert_response :success
+  end
+
+  test "should login an user by email" do
+      post sessions_url, params: {
+        login: @user.email,
+        password: 'password'
+      }
+
+    assert_redirected_to products_url
+  end
+
+  test "should login an user by username" do
+      post sessions_url, params: {
+        login: @user.username,
+        password: 'password'
+      }
+
+    assert_redirected_to products_url
+  end
+
+end
