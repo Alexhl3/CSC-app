a260c8028785a3dc128d63b9fd2f6b8619ceb1ae
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-08-18 12:27:33 -0400

    new MVC for the user with new form and minor fixes

---
+++ test/controllers/authentication/users_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class Authentication::UsersControllerTest < ActionDispatch::IntegrationTest
+  test "new_user_url" do
+    get new_user_url
+
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      post users_url, params: { user: { email: 'test@csc.com', username: 'testname', password: 'testpass' } }
+    end
+
+    assert_redirected_to products_url
+  end
+end
