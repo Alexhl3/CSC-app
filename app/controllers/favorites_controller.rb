bddb555df4e50276552a02eb51513801551cd906
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-08-23 15:46:48 -0400

    added option to delete user favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,11 @@ class FavoritesController < ApplicationController
     redirect_to product_path(product)
   end
 
+  def destroy
+    product.favorites.find_by(user: Current.user).destroy
+    redirect_to product_path(product)
+  end
+
   private
 
   def product
--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     login
-    @product = products(:machine5)
+    @product = products(:machine11)
+    @liked = products(:machine8)
   end
 
   test 'should create favorite' do
@@ -13,5 +14,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
 
     assert_redirected_to product_path(@product)
+  end
+
+  test 'should destroy favorite' do
+    assert_difference('Favorite.count', -1) do
+      delete favorite_url(@liked.id)
+    end
+
+
+    assert_redirected_to product_path(@liked)
   end
 end
