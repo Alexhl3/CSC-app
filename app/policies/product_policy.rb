f93f35c84af34f880a3427a2078e25623c67c8d6
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-09-07 10:49:58 -0300

    changed admin authorization and notifications

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,13 +1,13 @@
 class ProductPolicy < BasePolicy
   def edit
-    record.user_id == Current.user.id
+    (record.user_id == Current.user.id) or (Current.user.admin?)
   end
-
+  
   def update
-    record.user_id == Current.user.id
+    (record.user_id == Current.user.id) or (Current.user.admin?)
   end
-
+  
   def destroy
-    record.user_id == Current.user.id
+    (record.user_id == Current.user.id) or (Current.user.admin?)
   end
 end
