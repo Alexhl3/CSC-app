9af2653829d608a31fe625bca36486ba509fce9e
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-09-21 11:55:23 -0300

    added status option for products creation and editing form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -57,7 +57,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price, :photo, :category_id, images: [])
+    params.require(:product).permit(:title, :description, :price, :status, :photo, :category_id, images: [])
   end
 
   def product_params_index
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -34,6 +34,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         description: 'Maquina en buen estado.',
         price: 230,
         category_id: categories(:newMachines).id,
+        status: true,
         photo: fixture_file_upload('maquinaN1.png', 'image/png'),
         images: [fixture_file_upload('maquinaN1.png', 'image/png'), fixture_file_upload('maquinaN2.png', 'image/png')]
       }
