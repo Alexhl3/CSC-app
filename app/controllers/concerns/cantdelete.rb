a7c8bd56c45ffb2ce0a3b0d8076ba84ec9c9a6a9
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-09-30 11:05:55 -0300

    fix for cantdelete concern

--- app/controllers/concerns/cantdelete.rb
+++ app/controllers/concerns/cantdelete.rb
@@ -2,7 +2,7 @@ module Cantdelete
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::DeleteRestrictionError do
+    rescue_from "ActiveRecord::DeleteRestrictionError" do
       redirect_to products_path, alert: t('.cant_delete')
     end
   end
