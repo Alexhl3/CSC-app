feb169f778a1cf11dc6f5a9a644d3534bc7bdd4a
Alejandro <4l3j4ndr0.c1@gmail.com>
2022-10-17 17:38:55 -0300

    added only one dropdown button at a time and z-index position fixes

--- app/javascript/controllers/dropMenu_controller.js
+++ app/javascript/controllers/dropMenu_controller.js
@@ -4,6 +4,20 @@ export default class extends Controller {
   static targets = ["items"]
 
   open() {
-    this.itemsTarget.classList.toggle('hidden')
+    const active = document.querySelectorAll('.active');
+
+    if(this.itemsTarget.classList.contains('active')){
+      this.itemsTarget.classList.remove('active')
+      this.itemsTarget.classList.toggle('hidden')
+    }else if(active.length == 0){
+      this.itemsTarget.classList.add('active')
+      this.itemsTarget.classList.toggle('hidden')
+    }else{
+      active[0].classList.remove('active')
+      active[0].classList.toggle('hidden')
+      this.itemsTarget.classList.add('active')
+      this.itemsTarget.classList.toggle('hidden')
+    }
+
   }
 }
